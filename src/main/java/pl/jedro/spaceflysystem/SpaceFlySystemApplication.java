9c13859ce5222d25f5e3c188169064618f838dac
jedro <jttim23@gmail.com>
2020-03-20 18:50:03 +0100

    init

---
+++ src/main/java/pl/jedro/spaceflysystem/SpaceFlySystemApplication.java
@@ -0,0 +1,13 @@
+package pl.jedro.spaceflysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceFlySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceFlySystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jedro/spaceflysystem/SpaceFlySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jedro.spaceflysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceFlySystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
